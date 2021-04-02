8e781064ee9e9145dce544b01d3e113694928f70
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-04-02 16:26:59 +0300

    Add files via upload

---
+++ lab4/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _4_2
+{ 
+
+class Program
+    {
+
+        [DllImport("D:\\labs\\isp2sem\\lab4\\lab4_2\\Dll2\\x64\\Debug\\Dll2.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int FindMax(int[] a, int m);
+
+        [DllImport("D:\\labs\\isp2sem\\lab4\\lab4_2\\Dll2\\x64\\Debug\\Dll2.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern float Aver(int[] a, int m);
+    static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4 };
+         
+            Console.WriteLine("Average:" + Aver(array, array.Length));
+            Console.WriteLine("Max:" + FindMax(array, array.Length));
+        }
+    }
+}
