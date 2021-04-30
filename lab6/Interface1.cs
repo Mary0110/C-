4eed157fa2b66fb8b2e724514cf4d405a343e7a4
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-04-30 05:42:20 +0300

    Add files via upload

---
+++ lab6/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_3
+{
+    public interface IEntertainable
+    {
+        void EatOut();
+        void Celebrate();
+    }
+    public interface IComparable<T>
+    {
+        int CompareTo(T other);
+
+    }
+}
