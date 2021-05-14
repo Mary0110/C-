add284d8c5006ee74d6cb4629c07433ed0a27d57
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-05-14 04:54:42 +0300

    Add files via upload

---
+++ lab6/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_3
+{
+    public interface IEntertain
+    {
+        void EatOut();
+        void Celebrate();
+    }
+}
